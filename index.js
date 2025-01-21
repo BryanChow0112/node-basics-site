eac392fd9d72775eb2a5b60825277937ba363195
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2025-01-21 17:00:12 +0800

    Implement basic routing and HTTP status handling
    - Add routing logic to serve different HTML pages
    - Handle 200 status for successful requests
    - Set up 301 redirects for moved pages
    - Add 404 error handling for undefined routes

--- index.js
+++ index.js
@@ -5,14 +5,40 @@ const server = http.createServer((req, res) => {
   // Set the header content type
   res.setHeader("Content-Type", "text/html");
 
+  // Routing
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200; // OK (Success)
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301; // Moved Permanently (Redirect)
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404; // Not Found (Error)
+      break;
+  }
+
   // Send an HTML file as a response
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     } else {
-      res.write(data);
-      res.end();
+      //   res.write(data);
+      res.end(data);
     }
     res.end();
   });

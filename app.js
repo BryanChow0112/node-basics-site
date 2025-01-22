458e58396f25f1c1e1283ea97e767cd4459c26bd
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2025-01-22 15:30:45 +0800

    Implement MongoDB integration with Blog model
    - Install Mongoose for MongoDB connectivity
    - Create models directory for database schemas
    - Define Blog model with schema properties
    - Add test routes for database operations

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const path = require("node:path");
 const morgan = require("morgan");
-const app = express();
+const mongoose = require("mongoose");
+const Blog = require("./models/Blog");
 const PORT = 3000;
+
+// Create an Express app
+const app = express();
+
+// Connect to MongoDB
+const dbURI =
+  "mongodb+srv://odin:test1234@cluster0.ocby7.mongodb.net/node_learning?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000)) // Listen for requests on port 3000 after connected to MongoDB
+  .catch((err) => console.log(err));
 
 // Set the view engine to EJS
 app.set("view engine", "ejs");
@@ -15,43 +28,70 @@ app.use(express.static(path.join(__dirname, "public")));
 // HTTP request logger
 app.use(morgan("dev"));
 
+// Test routes for MongoDB
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog"
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("67909bcaa0e557f9351fdc33")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 // Page routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur"
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur"
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur"
-    }
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/contact", (req, res) => {
-  res.render("contact", { title: "Contact" });
-});
-
+// Blog routes
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { blogs: result, title: "All blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // 404 handler - must be last
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
-});
-
-// Listen for requests on port 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });

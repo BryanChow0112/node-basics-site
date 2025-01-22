46df87930aba062727288e1113ac2d821323f8db
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2025-01-22 12:07:43 +0800

    Migrate from static HTML to EJS templating
    - Install EJS template engine
    - Configure Express view engine for EJS
    - Convert HTML files to EJS format
    - Update routes to use render() instead of sendFile()
    - Remove redundant redirect routes

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 const express = require("express");
-
-// Create an Express app
 const app = express();
 const PORT = 3000;
+const path = require("node:path");
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+// Look for views in the views directory
+app.set("views", path.join(__dirname, "views"));
 
 // Page routes
 app.get("/", (req, res) => {
-  // Tells Express to look for the file in the current directory
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile("./views/contact-me.html", { root: __dirname });
+  res.render("contact", { title: "Contact" });
 });
 
-// Redirects
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 handler - must be last
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
 
 // Listen for requests on port 3000
